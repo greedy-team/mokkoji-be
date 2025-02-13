0ca38b862830ea59506d19158c35b56f931fb840
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-02-13 20:13:36 +0900

    Fix: @Repository 기입

--- src/main/java/com/greedy/mokkoji/db/club/repository/ClubRepository.java
+++ src/main/java/com/greedy/mokkoji/db/club/repository/ClubRepository.java
@@ -2,6 +2,8 @@ package com.greedy.mokkoji.db.club.repository;
 
 import com.greedy.mokkoji.db.club.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
 }
