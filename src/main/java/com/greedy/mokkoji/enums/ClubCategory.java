4b3300c55ee4b597fbdf7057413277c8183be4a3
davidolleh <asherolleh@gmail.com>
2025-02-16 19:29:38 +0900

    [Feat]: 동아리 소속, 카테고리 enum 값 추가

---
+++ src/main/java/com/greedy/mokkoji/enums/ClubCategory.java
@@ -0,0 +1,16 @@
+package com.greedy.mokkoji.enums;
+
+public enum ClubCategory {
+    // 문화/예술
+    CULTURE_ART,
+    // 체육
+    SPORTS,
+    // 봉사
+    VOLUNTEER,
+    // 종교
+    RELIGION,
+    // 학술/교양
+    ACADEMIC,
+    // 기타
+    ETC,
+}
