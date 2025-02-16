043f11f5da123ca3458b5691e90ccabcf6fad4ed
davidolleh <asherolleh@gmail.com>
2025-02-16 20:01:44 +0900

    [Feat]: 메일 알림 전송 기능 구현

--- src/main/java/com/greedy/mokkoji/db/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/greedy/mokkoji/db/favorite/repository/FavoriteRepository.java
@@ -4,10 +4,15 @@ import com.greedy.mokkoji.db.club.entity.Club;
 import com.greedy.mokkoji.db.favorite.entity.Favorite;
 import com.greedy.mokkoji.db.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    @Query("SELECT f FROM Favorite f JOIN FETCH f.user WHERE f.club.id = :clubId")
+    List<Favorite> findByClubIdWithFetchJoin(final Long clubId);
 
     boolean existsByUserAndClub(final User user, final Club club);
 
