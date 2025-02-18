414a51f3edc592c152d224a384b44f42983783e2
hyebin <shin378378@naver.com>
2025-02-19 07:04:51 +0900

    Refactor : 안 쓰는 의존성 제거하기

--- src/main/java/com/greedy/mokkoji/MokkojiApplication.java
+++ src/main/java/com/greedy/mokkoji/MokkojiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MokkojiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MokkojiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MokkojiApplication.class, args);
+    }
 
 }
