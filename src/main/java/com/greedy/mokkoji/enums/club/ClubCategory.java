a4652e879a622fc36ccd7f4c6f71c7d3a9f183a6
davidolleh <asherolleh@gmail.com>
2025-02-20 17:44:11 +0900

    [Refactor]: ENUM 이름 변경

--- src/main/java/com/greedy/mokkoji/enums/club/ClubCategory.java
+++ src/main/java/com/greedy/mokkoji/enums/club/ClubCategory.java
@@ -1,6 +1,18 @@
 package com.greedy.mokkoji.enums.club;
 
 public enum ClubCategory {
+    // 문화/예술
+    CULTURE_ARTS,
+    // 체육
+    SPORTS,
+    // 봉사
+    VOLUNTEER,
+    // 종교
+    RELIGION,
+    // 학술/교양
+    ACADEMIC_CULTURE,
+    // 기타
+    ETC,
 
     TEST
 }
