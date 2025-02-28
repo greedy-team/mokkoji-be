f813b42e359ea2a46f3103d072419372609bb114
davidolleh <asherolleh@gmail.com>
2025-02-28 20:34:57 +0900

    [REFACTOR]: bearer값 추출하는 역할 분리

--- src/main/java/com/greedy/mokkoji/api/auth/controller/argumentResolver/UserAuthArgumentResolver.java
+++ src/main/java/com/greedy/mokkoji/api/auth/controller/argumentResolver/UserAuthArgumentResolver.java
@@ -1,5 +1,6 @@
 package com.greedy.mokkoji.api.auth.controller.argumentResolver;
 
+import com.greedy.mokkoji.api.jwt.BearerAuthExtractor;
 import com.greedy.mokkoji.api.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,8 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Slf4j
 @RequiredArgsConstructor
 public class UserAuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final BearerAuthExtractor bearerAuthExtractor;
     private final JwtUtil jwtUtil;
 
     @Override
@@ -34,7 +37,9 @@ public class UserAuthArgumentResolver implements HandlerMethodArgumentResolver {
             final WebDataBinderFactory binderFactory
     ) throws Exception {
         final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        Long userId = jwtUtil.getUserIdFromToken(request.getHeader(HttpHeaders.AUTHORIZATION));
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String token = bearerAuthExtractor.extractTokenValue(authHeader);
+        final Long userId = jwtUtil.getUserIdFromToken(token);
         return new AuthCredential(userId);
     }
 }
---
+++ src/main/java/com/greedy/mokkoji/api/jwt/BearerAuthExtractor.java
@@ -0,0 +1,24 @@
+package com.greedy.mokkoji.api.jwt;
+
+import com.greedy.mokkoji.common.exception.MokkojiException;
+import com.greedy.mokkoji.enums.message.FailMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BearerAuthExtractor {
+    private static final String BEARER_TYPE = "Bearer";
+
+    public String extractTokenValue(final String header) {
+        if (header == null || header.isEmpty()) {
+            throw new MokkojiException(FailMessage.UNAUTHORIZED_EMPTY_HEADER);
+        }
+
+        if (!header.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
+            throw new MokkojiException(FailMessage.UNAUTHORIZED_INVALID_TOKEN);
+        }
+
+        return header.substring(BEARER_TYPE.length()).trim();
+    }
+}
