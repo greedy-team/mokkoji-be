3f2875cf169d1393eeb90a03f837a3f787d02854
davidolleh <asherolleh@gmail.com>
2025-03-03 17:37:06 +0900

    [Refactor]: token value가 빈값일때 auth 에러 던지도록 설정

--- src/main/java/com/greedy/mokkoji/api/jwt/BearerAuthExtractor.java
+++ src/main/java/com/greedy/mokkoji/api/jwt/BearerAuthExtractor.java
@@ -13,16 +13,24 @@ public class BearerAuthExtractor {
     private static final String BEARER_TYPE = "Bearer";
 
     public String extractTokenValue(final String bearerToken) {
-        if (bearerToken == null || bearerToken.isEmpty()) {
+        validateTokenValue(bearerToken);
+
+        validateBearerKey(bearerToken);
+
+        return bearerToken.substring(BEARER_TYPE.length()).trim();
+    }
+
+    private void validateTokenValue(final String bearerToken) {
+        if (bearerToken == null || bearerToken.isEmpty() || bearerToken.trim().equals(BEARER_TYPE)) {
             log.warn("Autherizaiton 헤더 값이 없습니다.");
             throw new MokkojiException(FailMessage.UNAUTHORIZED_EMPTY_HEADER);
         }
+    }
 
+    private void validateBearerKey(final String bearerToken) {
         if (!bearerToken.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
             log.warn("AuthHeader의 값이 Bearer로 시작하지 않습니다: {}", bearerToken);
             throw new MokkojiException(FailMessage.UNAUTHORIZED_INVALID_TOKEN);
         }
-
-        return bearerToken.substring(BEARER_TYPE.length()).trim();
     }
 }
