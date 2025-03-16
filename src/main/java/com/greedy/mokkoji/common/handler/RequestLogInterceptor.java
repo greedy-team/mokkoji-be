2a5176f0ce0ba3f67baabb61cc73a2470a88c147
davidolleh <asherolleh@gmail.com>
2025-03-16 19:04:14 +0900

    [Feat]: 로그에서 공통으로 보여주는 값 객체로 구현

--- src/main/java/com/greedy/mokkoji/common/handler/RequestLogInterceptor.java
+++ src/main/java/com/greedy/mokkoji/common/handler/RequestLogInterceptor.java
@@ -1,6 +1,6 @@
 package com.greedy.mokkoji.common.handler;
 
-import com.greedy.mokkoji.common.log.LogEntry;
+import com.greedy.mokkoji.common.log.CommonLogInformation;
 import com.greedy.mokkoji.common.log.query.QueryCounter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,8 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.net.URL;
 
 @Slf4j
 @Component
@@ -22,7 +20,7 @@ public class RequestLogInterceptor implements HandlerInterceptor {
     private static final double TIME_CONVERSION_MS_TO_SEC = 1000.0;
 
     private final QueryCounter queryCounter;
-    private final LogEntry logEntry;
+    private final CommonLogInformation commonLogInformation;
 
     @Override
     public boolean preHandle(
@@ -34,7 +32,7 @@ public class RequestLogInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        logEntry.setUrl(request.getRequestURI());
+        commonLogInformation.setUri(request.getRequestURI());
         return true;
     }
 
@@ -48,20 +46,23 @@ public class RequestLogInterceptor implements HandlerInterceptor {
         final Long queryCount = queryCounter.getCount();
         final double duration = (System.currentTimeMillis() - queryCounter.getTime()) / TIME_CONVERSION_MS_TO_SEC;
 
-        log.info("requestIdentifier: {}, URL: {} statusCode: {}",
-                logEntry.getRequestIdentifier(), logEntry.getUrl(), response.getStatus()
+        log.info("[{}] URI: {} statusCode: {}",
+                commonLogInformation.getRequestIdentifier(), commonLogInformation.getUri(), response.getStatus()
         );
         warnAboutQuery(queryCount, duration);
     }
 
     private void warnAboutQuery(Long queryCount, double duration) {
         if (queryCount > QUERY_COUNT_WARNING_STANDARD) {
-            log.error("requestIdentifier: {}, URL: {}, 하나의 요청에 쿼리가 10번 이상 날라 갔습니다. 쿼리 횟수: {}",
-                    logEntry.getRequestIdentifier(), logEntry.getUrl(), queryCount);
+            log.warn("[{}] URI: {}, 하나의 요청에 쿼리가 10번 이상 날라 갔습니다. 쿼리 횟수: {}",
+                    commonLogInformation.getRequestIdentifier(), commonLogInformation.getUri(), queryCount
+            );
         }
 
         if (duration > TOTAL_TIME_WARNING_STANDARD_MS) {
-            log.error("requestIdentifier: {}, URL: {}, 하나의 요청이 0.25s 이상 소요되었습니다. 실제 소요 시간: {}", logEntry.getRequestIdentifier(), logEntry.getUrl(), duration);
+            log.warn("[{}] URI: {}, 하나의 요청이 0.25s 이상 소요되었습니다. 실제 소요 시간: {}",
+                    commonLogInformation.getRequestIdentifier(), commonLogInformation.getUri(), duration
+            );
         }
     }
 }
