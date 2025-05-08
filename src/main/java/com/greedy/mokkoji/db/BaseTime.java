9efcf2d6d2896848b692296b0d53fac0690a0897
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-09 02:11:02 +0900

    Fix: 동아리 엔티티 필드 수정

--- src/main/java/com/greedy/mokkoji/db/BaseTime.java
+++ src/main/java/com/greedy/mokkoji/db/BaseTime.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -13,6 +14,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(name = "created_at", columnDefinition = "timestamp")
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", columnDefinition = "timestamp")
