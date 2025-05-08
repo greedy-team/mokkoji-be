b3435f236398582c11366a707b29fdc96bebf861
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-09 02:34:01 +0900

    Fix: 유저 엔티티 필드 수정

--- src/main/java/com/greedy/mokkoji/db/user/entity/User.java
+++ src/main/java/com/greedy/mokkoji/db/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.greedy.mokkoji.db.user.entity;
 
+import com.greedy.mokkoji.enums.user.UserRole;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -32,13 +33,18 @@ public class User {
     @Column(name = "email", columnDefinition = "varchar(50)", nullable = true)
     private String email;
 
+    @Column(name = "role", columnDefinition = "varchar(50)")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
     @Builder
-    public User(final String studentId, final String name, final String department, final String grade, final String email) {
+    public User(final String studentId, final String name, final String department, final String grade, final String email, final UserRole role) {
         this.studentId = studentId;
         this.name = name;
         this.department = department;
         this.grade = grade;
         this.email = email;
+        this.role = role;
     }
 
     public void updateEmail(String email) {
---
+++ src/main/java/com/greedy/mokkoji/enums/user/UserRole.java
@@ -0,0 +1,8 @@
+package com.greedy.mokkoji.enums.user;
+
+public enum UserRole {
+    CLUB_ADMIN,
+    GREEDY_ADMIN,
+    NORMAL,
+    CLUB_MASTER
+}
