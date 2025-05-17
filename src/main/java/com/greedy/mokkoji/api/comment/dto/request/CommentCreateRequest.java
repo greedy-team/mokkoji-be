142d250e4fe461b3e3818a8883030b599285476e
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-17 14:32:41 +0900

    Fix: 평점 유효성 검사 추가

--- src/main/java/com/greedy/mokkoji/api/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/greedy/mokkoji/api/comment/dto/request/CommentCreateRequest.java
@@ -1,6 +1,11 @@
 package com.greedy.mokkoji.api.comment.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 public record CommentCreateRequest(
+        @Min(0)
+        @Max(5)
         Double rate,
         String content
 ) {
