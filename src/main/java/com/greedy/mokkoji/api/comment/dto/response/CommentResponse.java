c8d1cda93a9899ec52e4ac9a13bfb2d280bb971c
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-17 14:42:52 +0900

    Fix: 댓글 평점 정수형 변경

--- src/main/java/com/greedy/mokkoji/api/comment/dto/response/CommentResponse.java
+++ src/main/java/com/greedy/mokkoji/api/comment/dto/response/CommentResponse.java
@@ -5,12 +5,12 @@ import java.time.LocalDateTime;
 public record CommentResponse(
         Long id,
         String content,
-        Double rate,
+        Integer rate,
         boolean isModified,
         LocalDateTime time,
         boolean isWriter
 ) {
-    public static CommentResponse of(final Long id, final String content, final Double rate, final boolean isModified, final LocalDateTime time, final boolean isWriter) {
+    public static CommentResponse of(final Long id, final String content, final Integer rate, final boolean isModified, final LocalDateTime time, final boolean isWriter) {
         return new CommentResponse(id, content, rate, isModified, time, isWriter);
     }
 }
