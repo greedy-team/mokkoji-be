49a3739d94b2a94dc2fc6a23d4a1cde3103158df
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-17 14:38:37 +0900

    Fix: 댓글 패키지명 수정

--- src/main/java/com/greedy/mokkoji/db/comment/repository/CommentRepository.java
+++ src/main/java/com/greedy/mokkoji/db/comment/repository/CommentRepository.java
@@ -1,7 +1,7 @@
-package com.greedy.mokkoji.db.commenmt.repository;
+package com.greedy.mokkoji.db.comment.repository;
 
 import com.greedy.mokkoji.db.club.entity.Club;
-import com.greedy.mokkoji.db.commenmt.entity.Comment;
+import com.greedy.mokkoji.db.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
