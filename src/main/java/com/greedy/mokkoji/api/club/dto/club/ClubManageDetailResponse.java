7e1185ff81ecb952dc63c4d2ea8a38507957f2f1
goldm0ng <jujs45177@gmail.com>
2025-05-17 13:43:06 +0900

    Feat: 관리 차원의 동아리 상세 조회 DTO 작성

---
+++ src/main/java/com/greedy/mokkoji/api/club/dto/club/ClubManageDetailResponse.java
@@ -0,0 +1,14 @@
+package com.greedy.mokkoji.api.club.dto.club;
+
+public record ClubManageDetailResponse(
+        String name,
+        String category,
+        String affiliation,
+        String description,
+        String logo,
+        String instagram
+) {
+    public static ClubManageDetailResponse of(final String name, final String category, final String affiliation, final String description, final String logo, final String instagram) {
+        return new ClubManageDetailResponse(name, category, affiliation, description, logo, instagram);
+    }
+}
