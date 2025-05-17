c1493e7e86ac004105e809c0cb5000e900527343
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-05-17 14:45:36 +0900

    Fix: 이미 존재하는 댓글인지 확인

--- src/main/java/com/greedy/mokkoji/db/comment/repository/CommentRepository.java
+++ src/main/java/com/greedy/mokkoji/db/comment/repository/CommentRepository.java
@@ -2,10 +2,13 @@ package com.greedy.mokkoji.db.comment.repository;
 
 import com.greedy.mokkoji.db.club.entity.Club;
 import com.greedy.mokkoji.db.comment.entity.Comment;
+import com.greedy.mokkoji.db.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByClub(final Club club);
+
+    boolean existsByClubAndUser(final Club club, final User user);
 }
