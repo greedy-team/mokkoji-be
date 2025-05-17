ebb6ab75335a9d5247a495337b2bda78047074d0
hyebin <shin378378@naver.com>
2025-05-18 00:12:03 +0900

    Feat : RecruitmentRequestDto 만들기

---
+++ src/main/java/com/greedy/mokkoji/db/recruitment/repository/RecruitmentImageRepository.java
@@ -0,0 +1,9 @@
+package com.greedy.mokkoji.db.recruitment.repository;
+
+import com.greedy.mokkoji.db.recruitment.entity.RecruitmentImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecruitmentImageRepository extends JpaRepository <RecruitmentImage, Long> {
+}
