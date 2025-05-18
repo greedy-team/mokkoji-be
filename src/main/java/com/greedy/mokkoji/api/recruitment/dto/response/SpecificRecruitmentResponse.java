45cac21ae94609dc7d3a29768c208bf7dc27cf20
hyebin <shin378378@naver.com>
2025-05-19 01:27:32 +0900

    Feat : 모든 동아리 모집글 조회

--- src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/SpecificRecruitmentResponse.java
+++ src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/SpecificRecruitmentResponse.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record SpecificRecruitmentResponse (
+public record SpecificRecruitmentResponse(
         Long id,
         String title,
         List<String> images,
         String content,
         LocalDate startDate,
         LocalDate endDate
-){
+) {
     public static SpecificRecruitmentResponse of(
             Long id,
             String title,
