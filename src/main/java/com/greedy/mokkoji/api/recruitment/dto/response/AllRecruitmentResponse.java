f6ef49ff8b68110b55dca361248820284e34cac7
hyebin <shin378378@naver.com>
2025-05-20 02:21:30 +0900

    Refactor : AllRecruitmentResponse dto 수정

--- src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/AllRecruitmentResponse.java
+++ src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/AllRecruitmentResponse.java
@@ -1,11 +1,14 @@
 package com.greedy.mokkoji.api.recruitment.dto.response;
 
 import com.greedy.mokkoji.api.club.dto.page.PageResponse;
+import com.greedy.mokkoji.enums.club.ClubAffiliation;
+import com.greedy.mokkoji.enums.club.ClubCategory;
 import com.greedy.mokkoji.enums.recruitment.RecruitStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+//Todo: UI가 어떻게 나올지 몰라 필요한 것 같아 보이는 DTO 필드는 다 넣어뒀어요. 나중에 필요없는 거 빼야할 거 같아요.
 public record AllRecruitmentResponse(
         List<AllRecruitmentResponse.Recruitment> recruitments,
         PageResponse page
@@ -15,15 +18,23 @@ public record AllRecruitmentResponse(
     }
 
     public record Recruitment(
-            Long clubId,
-            String clubName,
+            Club club,
             Long id,
             String title,
             LocalDateTime recruitStart,
             LocalDateTime recruitEnd,
             RecruitStatus status,
-            String firstImage,
             boolean isFavorite
+    ) {
+    }
+
+    public record Club(
+                               Long id,
+                               String name,
+                               String description,
+                               ClubCategory clubCategory,
+                               ClubAffiliation clubAffiliation,
+                               String imageURL
     ) {
     }
 }
