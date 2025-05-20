3d43481a9027891b3eac09ee110ed3cd3f7305c7
goldm0ng <jujs45177@gmail.com>
2025-05-20 16:17:35 +0900

    Fix: 권한 확인 로직 책임 분리 및 권한 수정

--- src/main/java/com/greedy/mokkoji/enums/user/UserRole.java
+++ src/main/java/com/greedy/mokkoji/enums/user/UserRole.java
@@ -1,8 +1,23 @@
 package com.greedy.mokkoji.enums.user;
 
+import com.greedy.mokkoji.db.club.entity.Club;
+import com.greedy.mokkoji.db.user.entity.User;
+
 public enum UserRole {
     CLUB_ADMIN,
     GREEDY_ADMIN,
     NORMAL,
-    CLUB_MASTER
+    CLUB_MASTER;
+
+    public boolean canRegisterClub() {
+        return this == CLUB_ADMIN || this == GREEDY_ADMIN;
+    }
+
+    public boolean canManageClub(User user, Club club) {
+        return switch (this) {
+            case CLUB_ADMIN -> true;
+            case CLUB_MASTER -> user.getStudentId().equals(club.getClubMasterStudentId());
+            default -> false;
+        };
+    }
 }
