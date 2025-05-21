fb620669fc3b2352f51c3c2cdbd22e933bf4de67
hyebin <shin378378@naver.com>
2025-05-21 17:06:42 +0900

    Refactor : dto 변수 바꾸기

--- src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/deleteRecruitment/DeleteRecruitmentResponse.java
+++ src/main/java/com/greedy/mokkoji/api/recruitment/dto/response/deleteRecruitment/DeleteRecruitmentResponse.java
@@ -3,11 +3,11 @@ package com.greedy.mokkoji.api.recruitment.dto.response.deleteRecruitment;
 import java.util.List;
 
 public record DeleteRecruitmentResponse(
-        Long recruitmentId,
+        Long id,
         List<String> deleteImageUrls
 ) {
-    public static DeleteRecruitmentResponse of(Long recruitmentId, List<String> deleteImageUrls) {
-        return new DeleteRecruitmentResponse(recruitmentId, deleteImageUrls);
+    public static DeleteRecruitmentResponse of(Long id, List<String> deleteImageUrls) {
+        return new DeleteRecruitmentResponse(id, deleteImageUrls);
     }
 }
 
