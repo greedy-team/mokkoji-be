55ec2b2a98dc8d0c508fd19292ca6c739e96187f
goldm0ng <jujs45177@gmail.com>
2025-07-19 04:38:17 +0900

    feat: 카카오로 요청하는 FeignClient 인터페이스 정의

---
+++ src/main/java/com/greedy/mokkoji/api/external/KakaoUserInfoClient.java
@@ -0,0 +1,16 @@
+package com.greedy.mokkoji.api.external;
+
+import com.greedy.mokkoji.api.user.dto.resopnse.KakaoUserInfoResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(value = "kakaoInfoClient", url = "${kakao.user-info-url}")
+public interface KakaoUserInfoClient {
+
+    @GetMapping
+    KakaoUserInfoResponse kakaoUserInfo(
+        @RequestHeader("Authorization") final String token,
+        @RequestHeader(name = "Content-type") final String contentType
+    );
+}
