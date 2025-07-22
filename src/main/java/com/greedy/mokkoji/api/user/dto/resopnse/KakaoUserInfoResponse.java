7057caa8afcf080701d380e0da4cd7f87b0a3076
hyebin <shin378378@naver.com>
2025-07-22 23:47:31 +0900

    Refactor : 사용자 정보 불러오는 dto 수정

--- src/main/java/com/greedy/mokkoji/api/user/dto/resopnse/KakaoUserInfoResponse.java
+++ src/main/java/com/greedy/mokkoji/api/user/dto/resopnse/KakaoUserInfoResponse.java
@@ -2,11 +2,19 @@ package com.greedy.mokkoji.api.user.dto.resopnse;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import java.util.Map;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoUserInfoResponse(
-    String id,
-    Map<String, Object> kakaoAccount
+        String id,
+        KakaoAccount kakaoAccount
 ) {
+    public record KakaoAccount(
+            Profile profile
+    ) {
+        public record Profile(
+                String nickname,
+                boolean isDefaultNickname
+        ) {}
+    }
 }
+
