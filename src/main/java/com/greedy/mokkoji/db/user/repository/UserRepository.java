3d2c9e0fac268e60b264608d94cdf1449125e0c1
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-08-01 19:03:00 +0900

    Hotfix: 사용하지 않는 필드 값을 이용하는 쿼리문 제거

--- src/main/java/com/greedy/mokkoji/db/user/repository/UserRepository.java
+++ src/main/java/com/greedy/mokkoji/db/user/repository/UserRepository.java
@@ -9,6 +9,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByStudentId(String studentId);
-
-    Optional<User> findByUniqueId(String uniqueId);
 }
