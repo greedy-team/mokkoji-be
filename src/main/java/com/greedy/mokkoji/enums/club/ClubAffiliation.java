8d6af62675cac0a31f1477a88eccdb4f69572ae0
hyebin <shin378378@naver.com>
2025-08-06 23:19:17 +0900

    Refactor : ClubAffiliation enum 수정

--- src/main/java/com/greedy/mokkoji/enums/club/ClubAffiliation.java
+++ src/main/java/com/greedy/mokkoji/enums/club/ClubAffiliation.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ClubAffiliation {
 
     CENTRAL_CLUB("중앙동아리"),
-    DEPARTMENT_CLUB("기타동아리");
+    DEPARTMENT_CLUB("기타동아리"),
+    SMALL_GROUP("소모임");
 
     private final String description;
 }
