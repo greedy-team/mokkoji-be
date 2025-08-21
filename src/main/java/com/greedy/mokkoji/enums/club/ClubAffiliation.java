f3971deba9dbf25b7beb338f91b0398b2b46bf28
goldm0ng <jujs45177@gmail.com>
2025-08-21 22:36:00 +0900

    refactor: 동아리 소속 Enum value 네이밍 수정

--- src/main/java/com/greedy/mokkoji/enums/club/ClubAffiliation.java
+++ src/main/java/com/greedy/mokkoji/enums/club/ClubAffiliation.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ClubAffiliation {
 
-    CENTRAL_CLUB("중앙동아리"),
-    DEPARTMENT_CLUB("기타동아리"),
+    CENTRAL_CLUB("중앙"),
+    DEPARTMENT_CLUB("정인준/가인준"),
     SMALL_GROUP("소모임");
 
     private final String description;
