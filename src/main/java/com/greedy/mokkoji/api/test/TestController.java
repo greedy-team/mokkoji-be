9067a1624314ec45765d1a1207fbbfbe23d4780b
hyebin <shin378378@naver.com>
2025-11-05 15:56:31 +0900

    Feat : TestController에 operation 추가하기

--- src/main/java/com/greedy/mokkoji/api/test/TestController.java
+++ src/main/java/com/greedy/mokkoji/api/test/TestController.java
@@ -1,6 +1,8 @@
 package com.greedy.mokkoji.api.test;
 
 import com.greedy.mokkoji.common.response.APISuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.prefix}/test")
 public class TestController {
 
+    @Operation(summary = "서버 상태 체크 API")
+    @ApiResponse(responseCode = "200", description = "서버 정상 동작")
     @GetMapping("/health-check")
     public ResponseEntity<APISuccessResponse<String>> healthCheck() {
         return APISuccessResponse.of(HttpStatus.OK, "OK");
