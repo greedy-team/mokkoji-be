4284a405f5b98ef0e68ebdbdea5e723e473a702e
hyebin <shin378378@naver.com>
2025-11-05 13:52:10 +0900

    Feat: Swagger 문서에서 @Authentication AuthCredential 파라미터 숨기기

--- src/main/java/com/greedy/mokkoji/config/swagger/SwaggerConfig.java
+++ src/main/java/com/greedy/mokkoji/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.greedy.mokkoji.config;
+package com.greedy.mokkoji.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -7,23 +7,27 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
 
+import java.util.Arrays;
+
+
+@Configuration
 @OpenAPIDefinition(
         servers = {
                 @Server(url = "http://43.200.8.39:8080/", description = "개발 서버"),
                 @Server(url = "http://localhost:8080/", description = "로컬 서버")
         }
 )
-@Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
         String jwt = "JWT";
 
-        // 보안 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
         SecurityScheme securityScheme = new SecurityScheme()
                 .name(jwt)
@@ -31,7 +35,6 @@ public class SwaggerConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
-        // 최종 OpenAPI 객체 반환
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes(jwt, securityScheme))
                 .info(apiInfo())
@@ -44,5 +47,35 @@ public class SwaggerConfig {
                 .description("mokkoji 서비스 API 명세서")
                 .version("1.0.0");
     }
-}
 
+    //Swagger 문서에서 @Authentication AuthCredential 파라미터 숨기기
+    @Bean
+    public OperationCustomizer hideAuthCredentialParameter() {
+        return (operation, handlerMethod) -> {
+
+            Arrays.stream(handlerMethod.getMethodParameters())
+                    .filter(this::isAuthCredentialParameter) // 필터 조건 따로 분리
+                    .findAny()
+                    .ifPresent(param -> removeAuthCredentialParameter(operation));
+
+            return operation;
+        };
+    }
+
+    //AuthCredential 타입인지 확인
+    private boolean isAuthCredentialParameter(MethodParameter param) {
+        return "AuthCredential".equals(param.getParameterType().getSimpleName());
+    }
+
+    //Swagger 문서에서 authCredential 파라미터 제거
+    private void removeAuthCredentialParameter(io.swagger.v3.oas.models.Operation operation) {
+        if (operation.getParameters() == null) {
+            return;
+        }
+        operation.getParameters().removeIf(p ->
+                "authCredential".equalsIgnoreCase(p.getName())
+        );
+    }
+
+
+}
