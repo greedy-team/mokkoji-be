143acf355fcb55ca84d0e1bcb02cd8fd76a89280
hyebin <shin378378@naver.com>
2025-11-05 12:49:09 +0900

    Feat: ReportController에 operation 추가

--- src/main/java/com/greedy/mokkoji/api/report/controller/ReportController.java
+++ src/main/java/com/greedy/mokkoji/api/report/controller/ReportController.java
@@ -6,6 +6,12 @@ import com.greedy.mokkoji.api.report.dto.request.ReportRequest;
 import com.greedy.mokkoji.api.report.service.ReportService;
 import com.greedy.mokkoji.common.response.APISuccessResponse;
 import com.greedy.mokkoji.enums.report.ReportType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +24,26 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    @Operation(
+            summary = "신고 생성 API",
+            description = """
+                    사용자가 특정 대상을 신고합니다.  
+                    reportType은 다음 중 하나를 입력해야 합니다:  
+                    - CLUB (동아리)  
+                    - RECRUITMENT (모집글)  
+                    - COMMENT (댓글)  
+                    - RATING (평점)
+                    """,
+            security = {@SecurityRequirement(name = "JWT")}
+    )
+    @ApiResponse(responseCode = "201", description = "신고 생성 성공")
+    @Parameter(
+            name = "reportType",
+            description = "신고 대상 유형",
+            in = ParameterIn.QUERY,
+            required = true,
+            schema = @Schema(implementation = ReportType.class, example = "COMMENT")
+    )
     @PostMapping
     public ResponseEntity<APISuccessResponse<Void>> createReport(
             @Authentication final AuthCredential authCredential,
