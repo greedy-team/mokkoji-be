b589c862d7f4d98f717e555a3b803277a60ad02c
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2025-11-12 01:22:46 +0900

    Refactor : CommentController와 swagger 분리하기

--- src/main/java/com/greedy/mokkoji/api/comment/controller/CommentController.java
+++ src/main/java/com/greedy/mokkoji/api/comment/controller/CommentController.java
@@ -7,11 +7,6 @@ import com.greedy.mokkoji.api.comment.dto.request.CommentUpdateRequest;
 import com.greedy.mokkoji.api.comment.dto.response.CommentListResponse;
 import com.greedy.mokkoji.api.comment.service.CommentService;
 import com.greedy.mokkoji.common.response.APISuccessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,12 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.prefix}/comments")
-public class CommentController {
+public class CommentController implements CommentControllerSwagger {
 
     private final CommentService commentService;
 
-    @Operation(summary = "댓글 생성 API", description = " 평점(`rate`)은 1~5 사이의 정수 값이어야 합니다.", security = @SecurityRequirement(name = "JWT"))
-    @ApiResponse(responseCode = "201", description = "댓글 작성 성공")
     @PostMapping("/{clubId}")
     public ResponseEntity<APISuccessResponse<Void>> createComment(
             @PathVariable(name = "clubId") final Long clubId,
@@ -42,14 +35,6 @@ public class CommentController {
         return APISuccessResponse.of(HttpStatus.CREATED, null);
     }
 
-    @Operation(summary = "댓글 목록 조회 API", security = @SecurityRequirement(name = "JWT"))
-    @ApiResponse(responseCode = "200", description = "댓글 목록 조회 성공")
-    @Parameter(
-            name = "clubId",
-            description = "조회할 동아리 ID",
-            in = ParameterIn.PATH,
-            required = true
-    )
     @GetMapping("/{clubId}")
     public ResponseEntity<APISuccessResponse<CommentListResponse>> getComments(
             @PathVariable(name = "clubId") final Long clubId,
@@ -61,8 +46,6 @@ public class CommentController {
         );
     }
 
-    @Operation(summary = "댓글 수정 API", description = " 평점(`rate`)은 1~5 사이의 정수 값이어야 합니다.", security = @SecurityRequirement(name = "JWT"))
-    @ApiResponse(responseCode = "200", description = "댓글 수정 성공")
     @PatchMapping("/{commentId}")
     public ResponseEntity<APISuccessResponse<Void>> updateComment(
             @PathVariable(name = "commentId") final Long commentId,
@@ -78,8 +61,6 @@ public class CommentController {
         return APISuccessResponse.of(HttpStatus.OK, null);
     }
 
-    @Operation(summary = "댓글 삭제 API", security = @SecurityRequirement(name = "JWT"))
-    @ApiResponse(responseCode = "200", description = "댓글 삭제 성공")
     @DeleteMapping("/{commentId}")
     public ResponseEntity<APISuccessResponse<Void>> deleteComment(
             @PathVariable(name = "commentId") final Long commentId,
