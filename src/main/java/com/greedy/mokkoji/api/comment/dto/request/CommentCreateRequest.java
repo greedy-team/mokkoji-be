8c28125f9c263a180890e7719dca00feb229a3ec
gjtjrl303 <gjtjrl303@naver.com>
2025-11-30 17:46:39 +0900

    Refactor: validation 범위 명시

--- src/main/java/com/greedy/mokkoji/api/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/greedy/mokkoji/api/comment/dto/request/CommentCreateRequest.java
@@ -4,9 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-@Schema(description = "댓글 작성 요청")
 public record CommentCreateRequest(
-        @Schema(description = "평점", example = "5") @Min(0) @Max(5) Double rate,
+        @Schema(example = "5",minimum = "0", maximum = "5") @Min(0) @Max(5) Double rate,
         @Schema(description = "댓글 내용", example = "최고의 동아리!") String content
 ) {
 }
