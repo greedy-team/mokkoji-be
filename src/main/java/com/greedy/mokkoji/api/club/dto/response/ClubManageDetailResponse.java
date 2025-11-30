4e22c5de8edaef89ffefb63ae6aa832bfd4ab310
gjtjrl303 <gjtjrl303@naver.com>
2025-11-30 17:35:32 +0900

    Refactor: ClubManageDetailResponse의 category, affiliation 예시 값을 enum name으로 변경

--- src/main/java/com/greedy/mokkoji/api/club/dto/response/ClubManageDetailResponse.java
+++ src/main/java/com/greedy/mokkoji/api/club/dto/response/ClubManageDetailResponse.java
@@ -6,12 +6,12 @@ import lombok.Builder;
 @Builder
 @Schema(description = "동아리 관리 상세 정보 응답")
 public record ClubManageDetailResponse(
-        @Schema(description = "동아리명", example = "그리디") String name,
-        @Schema(description = "동아리 카테고리", example = "학술/교양") String category,
-        @Schema(description = "동아리 소속", example = "정인준/가인준") String affiliation,
-        @Schema(description = "동아리 소개", example = "세종대 최고의 코딩 동아리") String description,
-        @Schema(description = "동아리 로고", example = "greedy_logo.jpg") String logo,
-        @Schema(description = "인스타그램 URL", example = "https://instagram.com/greedy_sejong") String instagram
+        @Schema(example = "그리디") String name,
+        @Schema(example = "ACADEMIC_CULTURAL") String category,
+        @Schema(example = "DEPARTMENT_CLUB") String affiliation,
+        @Schema(example = "세종대 최고의 코딩 동아리") String description,
+        @Schema(example = "greedy_logo.jpg") String logo,
+        @Schema(example = "https://instagram.com/greedy_sejong") String instagram
 ) {
     public static ClubManageDetailResponse of(final String name, final String category, final String affiliation, final String description, final String logo, final String instagram) {
         return new ClubManageDetailResponse(name, category, affiliation, description, logo, instagram);
