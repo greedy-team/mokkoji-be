4c13ffc8c9cdeb3953ad252b43f4d063014c5b79
gjtjrl303 <gjtjrl303@naver.com>
2025-12-01 09:59:45 +0900

    Refactor: 페이지 시작 번호를 0에서 1로 변경

--- src/main/java/com/greedy/mokkoji/api/pagination/dto/PageResponse.java
+++ src/main/java/com/greedy/mokkoji/api/pagination/dto/PageResponse.java
@@ -6,10 +6,10 @@ import lombok.Builder;
 @Builder
 @Schema(description = "페이지네이션 정보")
 public record PageResponse(
-        @Schema(description = "현재 페이지 번호", example = "0") int page,
-        @Schema(description = "페이지 크기", example = "10") int size,
-        @Schema(description = "전체 페이지 수", example = "5") int totalPages,
-        @Schema(description = "전체 요소 수", example = "50") int totalElements
+        @Schema(example = "1") int page,
+        @Schema(example = "10") int size,
+        @Schema(example = "5") int totalPages,
+        @Schema(example = "50") int totalElements
 ) {
     public static PageResponse of(final int page, final int size, final int totalPages, final int totalElements) {
         return PageResponse.builder()
