8a157146266f537919f5cd1a0e0d6e4048155d2c
gjtjrl303 <gjtjrl303@naver.com>
2026-02-09 19:32:11 +0900

    Refactor: 코드 스타일 통일

--- src/main/java/com/greedy/mokkoji/api/report/controller/ReportController.java
+++ src/main/java/com/greedy/mokkoji/api/report/controller/ReportController.java
@@ -9,7 +9,11 @@ import com.greedy.mokkoji.enums.report.ReportType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
