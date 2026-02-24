fd4ba71a9d70cad9c115bc3d945cfafa500c220c
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-02-24 20:12:23 +0900

    Refactor : 자동정렬

--- src/test/java/com/greedy/mokkoji/club/service/ClubServiceTest.java
+++ src/test/java/com/greedy/mokkoji/club/service/ClubServiceTest.java
@@ -42,10 +42,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.anyLong;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @DisplayName("동아리 검색 및 필터링 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
