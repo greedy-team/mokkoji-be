9cdd4d430ab25872d093931396386a6bca36da24
goldm0ng <jujs45177@gmail.com>
2026-05-11 00:23:52 +0900

    refactor: `UniversityCode`에서 학교명 삭제

--- src/main/java/com/greedy/mokkoji/enums/university/UniversityCode.java
+++ src/main/java/com/greedy/mokkoji/enums/university/UniversityCode.java
@@ -6,10 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UniversityCode {
-    SEJONG("세종대학교"),
-    KONKUK("건국대학교"),
-    HANYANG("한양대학교"),
-    NO_CHOSEN("선택 안 함");
-
-    private final String universityName;
+    SEJONG,
+    KONKUK,
+    HANYANG,
+    NO_CHOSEN;
 }
