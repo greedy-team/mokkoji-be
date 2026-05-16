3f89e5d5f1c0fe902cf28d7193b164936ab0fe1b
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-16 22:49:31 +0900

    Feat : JWT 토큰에 학교와 권한 정보 추가

--- src/main/java/com/greedy/mokkoji/api/auth/controller/argumentResolver/AuthCredential.java
+++ src/main/java/com/greedy/mokkoji/api/auth/controller/argumentResolver/AuthCredential.java
@@ -1,6 +1,11 @@
 package com.greedy.mokkoji.api.auth.controller.argumentResolver;
 
+import com.greedy.mokkoji.enums.auth.AuthRole;
+import com.greedy.mokkoji.enums.university.UniversityCode;
+
 public record AuthCredential(
-        Long userId
+        Long userId,
+        UniversityCode universityCode,
+        AuthRole authRole
 ) {
 }
