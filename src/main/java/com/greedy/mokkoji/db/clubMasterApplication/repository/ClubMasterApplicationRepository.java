939aada73832c2ecf6f7281b261f68931a2d4413
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-23 22:33:23 +0900

    Feat : ClubMasterApplicationRepository 생성

---
+++ src/main/java/com/greedy/mokkoji/db/clubMasterApplication/repository/ClubMasterApplicationRepository.java
@@ -0,0 +1,7 @@
+package com.greedy.mokkoji.db.clubMasterApplication.repository;
+
+import com.greedy.mokkoji.db.clubMasterApplication.entity.ClubMasterApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubMasterApplicationRepository extends JpaRepository<ClubMasterApplication, Long> {
+}
