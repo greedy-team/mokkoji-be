2754f9c2feeb3a5c620ff51ecc4c74e1fa457572
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-23 22:33:14 +0900

    Feat : ClubMasterApplication 엔티티 생성

---
+++ src/main/java/com/greedy/mokkoji/db/clubMasterApplication/entity/ClubMasterApplication.java
@@ -0,0 +1,72 @@
+package com.greedy.mokkoji.db.clubMasterApplication.entity;
+
+import com.greedy.mokkoji.db.BaseTime;
+import com.greedy.mokkoji.db.club.entity.Club;
+import com.greedy.mokkoji.db.university.entity.University;
+import com.greedy.mokkoji.db.user.entity.User;
+import com.greedy.mokkoji.enums.application.ApplicationStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "club_master_application")
+public class ClubMasterApplication extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "bigint", nullable = false)
+    private Long id;
+
+    @JoinColumn(name = "university_id", columnDefinition = "bigint", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private University university;
+
+    @JoinColumn(name = "club_id", columnDefinition = "bigint", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Club club;
+
+    @JoinColumn(name = "user_id", columnDefinition = "bigint")
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "user_name", columnDefinition = "varchar(50)", nullable = false)
+    private String userName;
+
+    @Column(name = "status", columnDefinition = "varchar(20)", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private ApplicationStatus status;
+
+    @Column(name = "reject_reason", columnDefinition = "text")
+    private String rejectReason;
+
+    @Builder
+    public ClubMasterApplication(
+            final University university,
+            final Club club,
+            final User user,
+            final String userName,
+            final String rejectReason
+    ) {
+        this.university = university;
+        this.club = club;
+        this.user = user;
+        this.userName = userName;
+        this.status = ApplicationStatus.PENDING;
+        this.rejectReason = rejectReason;
+    }
+
+    public void approve() {
+        this.status = ApplicationStatus.APPROVED;
+        this.rejectReason = null;
+    }
+
+    public void reject(final String rejectReason) {
+        this.status = ApplicationStatus.REJECTED;
+        this.rejectReason = rejectReason;
+    }
+}
