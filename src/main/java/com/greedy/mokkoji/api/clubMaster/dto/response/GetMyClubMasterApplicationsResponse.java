5cb061d278a830123b45edc0900e6c5da786d838
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-24 03:05:41 +0900

    Feat: ClubMasterController dto 예시 추가

--- src/main/java/com/greedy/mokkoji/api/clubMaster/dto/response/GetMyClubMasterApplicationsResponse.java
+++ src/main/java/com/greedy/mokkoji/api/clubMaster/dto/response/GetMyClubMasterApplicationsResponse.java
@@ -1,17 +1,18 @@
 package com.greedy.mokkoji.api.clubMaster.dto.response;
 
 import com.greedy.mokkoji.enums.application.ApplicationStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 
 public record GetMyClubMasterApplicationsResponse(
-        Long id,
-        String universityName,
-        String clubName,
-        String userName,
-        ApplicationStatus status,
-        String rejectReason,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
+        @Schema(example = "1") Long id,
+        @Schema(example = "세종대") String universityName,
+        @Schema(example = "그리디") String clubName,
+        @Schema(example = "김세종") String userName,
+        @Schema(example = "PENDING") ApplicationStatus status,
+        @Schema(example = "실명을 적어주세요.") String rejectReason,
+        @Schema(example = "2026-04-22T14:00:00") LocalDateTime createdAt,
+        @Schema(example = "2026-04-23T14:00:00") LocalDateTime updatedAt
 ) {
 }
