a58486020a2d4259009a7b3a64ba5d721db2a9bd
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-24 03:05:49 +0900

    Feat: AdminController dto 예시 추가

--- src/main/java/com/greedy/mokkoji/api/admin/dto/request/RejectClubMasterApplicationRequest.java
+++ src/main/java/com/greedy/mokkoji/api/admin/dto/request/RejectClubMasterApplicationRequest.java
@@ -1,6 +1,8 @@
 package com.greedy.mokkoji.api.admin.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RejectClubMasterApplicationRequest(
-        String rejectReason
+        @Schema(example = "실명을 적어주세요.") String rejectReason
 ) {
 }
