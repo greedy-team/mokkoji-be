3931b3502157757076f13a41a8863c5f789cb953
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-05-25 17:56:36 +0900

    Feat: ClubMasterTransfer 레포지토리 생성

---
+++ src/main/java/com/greedy/mokkoji/db/clubMasterTransfer/repository/ClubMasterTransferRepository.java
@@ -0,0 +1,7 @@
+package com.greedy.mokkoji.db.clubMasterTransfer.repository;
+
+import com.greedy.mokkoji.db.clubMasterTransfer.entity.ClubMasterTransfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubMasterTransferRepository extends JpaRepository<ClubMasterTransfer, Long> {
+}
