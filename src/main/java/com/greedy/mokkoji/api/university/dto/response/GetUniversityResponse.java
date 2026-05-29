b3e3362fb50e108dbb7cb1993a40bdd50cd73e58
goldm0ng <jujs45177@gmail.com>
2026-05-29 14:37:42 +0900

    Refactor: 학교 코드 응답 대문자로 적용

--- src/main/java/com/greedy/mokkoji/api/university/dto/response/GetUniversityResponse.java
+++ src/main/java/com/greedy/mokkoji/api/university/dto/response/GetUniversityResponse.java
@@ -6,14 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record GetUniversityResponse(
         @Schema(example = "1") Long id,
         @Schema(example = "세종대학교") String name,
-        @Schema(example = "sejong") String code,
+        @Schema(example = "SEJONG") String code,
         @Schema(example = "https://mokkoji-app-data.s3.ap-northeast-2.amazonaws.com/university-logo/2/sejong.jpg") String logo
 ) {
     public static GetUniversityResponse from(final University university) {
         return new GetUniversityResponse(
                 university.getId(),
                 university.getName(),
-                university.getCode().name().toLowerCase(),
+                university.getCode().name(),
                 university.getLogo()
         );
     }
