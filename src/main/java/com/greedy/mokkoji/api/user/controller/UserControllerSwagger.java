a6cc242453e63f28e60ec7f3edee0845b3f14e43
gjtjrl303 <gjtjrl303@naver.com>
2026-06-19 11:14:27 +0900

    Refactor: 대학 변경 시 즐겨찾기 삭제에 따른 swagger 수정

--- src/main/java/com/greedy/mokkoji/api/user/controller/UserControllerSwagger.java
+++ src/main/java/com/greedy/mokkoji/api/user/controller/UserControllerSwagger.java
@@ -60,7 +60,7 @@ public interface UserControllerSwagger {
 
     @Operation(
             summary = "사용자 정보 수정 API",
-            description = "전달된 필드만 수정됩니다.",
+            description = "전달된 필드만 수정됩니다. 대학교가 변경되는 경우 기존 즐겨찾기는 모두 삭제됩니다.",
             security = {@SecurityRequirement(name = "JWT")}
     )
     @ApiResponse(responseCode = "200", description = "사용자 정보 수정 성공")
