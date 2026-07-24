e2939872ed1a30ee222c1db9cca8a3f17b6849f9
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-07-25 04:39:31 +0900

    Test: 통합 테스트용 Testcontainers(MySQL) 도입

--- src/test/java/com/greedy/mokkoji/common/AbstractTest.java
+++ src/test/java/com/greedy/mokkoji/common/AbstractTest.java
@@ -9,9 +9,31 @@ import com.greedy.mokkoji.db.university.repository.UniversityRepository;
 import com.greedy.mokkoji.db.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
 
 @ActiveProfiles("test")
 public class AbstractTest {
+
+    // 통합 테스트 전용 MySQL 컨테이너(dev와 동일한 MySQL 엔진).
+    protected static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("mokkoji_test")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        MYSQL.start();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> "root");
+        registry.add("spring.datasource.password", MYSQL::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+    }
+
     @Autowired
     protected UserRepository userRepository;
 
