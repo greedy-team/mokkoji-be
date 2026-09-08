4dc598e8b784be170d9f42c3a21c8dbfcbeea5c0
goldm0ng <jujs45177@gmail.com>
2026-09-08 15:53:39 +0900

    Feat: 관리자 및 사용자 로그인 응답에 `role`, `universityCode` 추가

--- src/main/java/com/greedy/mokkoji/api/user/dto/resopnse/LoginResponse.java
+++ src/main/java/com/greedy/mokkoji/api/user/dto/resopnse/LoginResponse.java
@@ -1,5 +1,7 @@
 package com.greedy.mokkoji.api.user.dto.resopnse;
 
+import com.greedy.mokkoji.enums.university.UniversityCode;
+import com.greedy.mokkoji.enums.user.UserRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
@@ -7,13 +9,23 @@ import lombok.Builder;
 public record LoginResponse(
         @Schema(example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...") String accessToken,
         @Schema(example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...") String refreshToken,
-        @Schema(example = "false") Boolean isNewUser
+        @Schema(example = "false") Boolean isNewUser,
+        @Schema(example = "NORMAL") UserRole role,
+        @Schema(example = "SEJONG") UniversityCode universityCode
 ) {
-    public static LoginResponse of(final String accessToken, final String refreshToken, final Boolean isNewUser) {
+    public static LoginResponse of(
+            final String accessToken,
+            final String refreshToken,
+            final Boolean isNewUser,
+            final UserRole role,
+            final UniversityCode universityCode
+    ) {
         return LoginResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .isNewUser(isNewUser)
+                .role(role)
+                .universityCode(universityCode)
                 .build();
     }
 }
